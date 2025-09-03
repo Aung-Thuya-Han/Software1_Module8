bceaa66ae7781486f19e2d11b23d4581c488954b
aungthuyahan <aungthu.ya659@gmail.com>
2025-09-03 13:14:28 +0630

    Final Commit!

--- module8.3.py
+++ module8.3.py
@@ -3,7 +3,6 @@ from geopy.distance import geodesic
 
 def getting_distance (user_input):
 
-    while user_input != "":
         sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{user_input}'"
         cursor = connection.cursor()
         cursor.execute(sql)
@@ -20,9 +19,20 @@ connection = mysql.connector.connect(
          )
 
 first_code = input("Enter first airport code: ")
-first_airport = getting_distance(first_code)
 second_code = input("Enter second airport code: ")
-second_airport = getting_distance(second_code)
 
-distance_between_airports = geodesic(first_airport, second_airport).km
-print(f"The distance between two airports is {distance_between_airports:.3f} kilometers.")
+while first_code != '' or second_code != '':
+
+    first_airport = getting_distance(first_code)
+    second_airport = getting_distance(second_code)
+    distance_between_airports = geodesic(first_airport, second_airport).km
+    print(f"The distance between two airports is {distance_between_airports:.3f} kilometers.")
+
+    first_code = input("Enter first airport code: ")
+    second_code = input("Enter second airport code: ")
+
+else:
+    print("Please enter a valid airport code.")
+
+
+
